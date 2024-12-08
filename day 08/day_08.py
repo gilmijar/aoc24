f1e471fe5abce0ca772d1a5c1665e4a74761e049
Ilia Gilmijarow <gilmijar@gmail.com>
2024-12-08 14:44:37 +0100

    day 8 part 1 done

--- day 08/day_08.py
+++ day 08/day_08.py
@@ -1,9 +1,10 @@
 def calc_bb(point, bounds):
-    distances = min(point[0], bounds[0] - point[0]), min(point[1], bounds[1] - point[1])
+    distances = min(point[0], bounds[0] - 1 - point[0]), min(point[1], bounds[1] - 1 - point[1])
     bb = {
         'rows': {'min': point[0]-distances[0], 'max': point[0]+distances[0]},
         'cols': {'min': point[1]-distances[1], 'max': point[1]+distances[1]}
     }
+    return bb
 
 
 def in_bb(point, bb):
@@ -11,15 +12,34 @@ def in_bb(point, bb):
            bb['cols']['min'] <= point[1] <= bb['cols']['max']
 
 
-filename = 'test'
-# filename = 'input'
+def calc_antinode(point, other):
+    r1, c1 = point
+    r2, c2 = other
+    r = r1 - r2 + r1
+    c = c1 - c2 + c1
+    return r, c
+
+
+# filename = 'test'
+filename = 'input'
 
 file = open(filename, 'r').read()
+lines = file.splitlines()
+max_row = len(lines)
+max_col = len(lines[0])
 frequencies = set(file) - set('\n#.')
 locations = {}
 for freq in frequencies:
     locations[freq] = [(r, c)
-                       for r, row in enumerate(file.splitlines())
+                       for r, row in enumerate(lines)
                        for c, symbol in enumerate(row) if symbol == freq]
 
-print(locations)
+focals = set()
+for freq in frequencies:
+    for antenna in locations[freq]:
+        a_bb = calc_bb(antenna, (max_row, max_col))
+        antennae = locations[freq][:]
+        antennae.remove(antenna)
+        focals.update(calc_antinode(antenna, other_antenna) for other_antenna in antennae if in_bb(other_antenna, a_bb))
+
+print(len(focals))
