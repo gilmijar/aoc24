986e9ea76f050ec065b6217e20a7b0cbd13ad687
Ilia Gilmijarow <gilmijar@gmail.com>
2024-12-10 13:00:47 +0100

    day 10 part 2 done!

--- day 10/day_10.py
+++ day 10/day_10.py
@@ -3,9 +3,10 @@
 filename = 'input'
 
 
-def explore(terr: dict, pos: complex):
+def explore(terr: dict, pos: complex, no_repeats: bool = True):
     height = terr[pos]
-    del terr[pos]
+    if no_repeats:
+        del terr[pos]
     if height == 9:
         return 1
     else:
@@ -14,7 +15,7 @@ def explore(terr: dict, pos: complex):
             direction = pos + heading
             try:
                 if terr[direction] == height + 1:
-                    trails += explore(terr, direction)
+                    trails += explore(terr, direction, no_repeats)
             except KeyError:
                 pass
         return trails
@@ -27,6 +28,13 @@ terrain = {r + c * 1j: int(v) for r, row in enumerate(data) for c, v in enumerat
 
 # find all 0s, but now let's test for ixed pos (0, 0)
 t_heads = [k for k, v in terrain.items() if v == 0]
+# Since we mutate the terrain map for with each step - every starting point gets its own copy
 scores = [explore(dict(terrain), t_h) for t_h in t_heads]
+print(scores)
+print(sum(scores))
+
+print('\n   Part 2 \n')
+
+scores = [explore(terrain, t_h, no_repeats=False) for t_h in t_heads]
 print(scores)
 print(sum(scores))
