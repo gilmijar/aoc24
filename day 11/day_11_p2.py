4d22e483422fe90ad6a32e6a7317c511e6136629
Ilia Gilmijarow <gilmijar@gmail.com>
2024-12-12 00:23:45 +0100

    day 11 part 2 done

--- day 11/day_11_p2.py
+++ day 11/day_11_p2.py
@@ -1,25 +1,12 @@
+# It also can solve part 1
 from math import log10
-from array import array
-from multiprocessing import Pool
 from functools import partial
 from time import monotonic
 from sys import argv
+from functools import lru_cache
 
 
-def blink(stones):
-    new_stones = []  # array('Q')  # unsigned long long (8 bytes int)
-    for stn in stones:
-        if stn == 0:
-            new_stones.append(1)
-        elif int(log10(stn)) % 2 != 1:  # even-digit numbers will have odd log10 integer part
-            new_stones.append(stn * 2024)
-        else:
-            new_stones.extend(
-                divmod(stn, 10 ** ((int(log10(stn)) + 1) // 2))
-            )
-    return new_stones
-
-
+@lru_cache(30000)
 def follow_stone(stone: int, depth: int):
     while stone == 0 or int(log10(stone)) % 2 != 1:  # even-digit numbers will have odd log10 integer part
         if stone == 0:
@@ -37,26 +24,14 @@ def follow_stone(stone: int, depth: int):
 
 
 if __name__ == '__main__':
-    BLINKS = int(argv[1]) if len(argv) == 2 else 42
+    BLINKS = int(argv[1]) if len(argv) == 2 else 25
     # filename = 'test'
     filename = 'input'
     data = open(filename, 'r').read().strip().split()
     stones = [int(st) for st in data]
     d = BLINKS
     t0 = monotonic()
-    # Expand the list of stones,
-    # so that we have more individual stones to feed the recurive counter
-    while monotonic() - t0 < 20:
-        stones = blink(stones)
-        d -= 1
-    print(f'Elapsed: {monotonic() - t0} seconds')
-    print(BLINKS - d, 'generations.', 'Stones after expansion:', len(stones))
-    print('I shall begin commencing...')
     follow_2 = partial(follow_stone, depth=d)
-    P = Pool(4)
-    t0 = monotonic()
-    # final_stones = map(follow_2, stones)  # This is for comparison - single-process
-    final_stones = P.imap_unordered(follow_2, stones, 1_000_000)
+    final_stones = map(follow_2, stones)
     print(sum(final_stones))
     print(f'Elapsed: {monotonic() - t0} seconds')
-
