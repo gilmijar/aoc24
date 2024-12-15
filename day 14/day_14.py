507a0e704481377c60b58572142ec63bd7a501e9
Ilia Gilmijarow <gilmijar@gmail.com>
2024-12-15 03:12:45 +0100

    day 14 part 2 in progress

--- day 14/day_14.py
+++ day 14/day_14.py
@@ -1,3 +1,5 @@
+from sys import argv
+
 def parse_robot(ln):
     halves = ln.replace('p=', '').replace('v=', '').split()
     posx, posy = (int(x) for x in halves[0].split(','))
@@ -31,12 +33,23 @@ if __name__ == '__main__':
     print(len(quad_0_0), len(quad_1_0), len(quad_0_1), len(quad_1_1))
     print(len(quad_0_0) * len(quad_1_0) * len(quad_0_1) * len(quad_1_1))
 
-    # for y in range(int(tiles.imag)):
-    #     print()
-    #     print(' ' * 35)
-    #     for x in range(int(tiles.real)):
-    #         c = new_robots.count(x + y*1j)
-    #         if c > 0:
-    #             print(c, end=' ')
-    #         else:
-    #             print('. ', end=' ')
+    vertical = [complex(half_x, y) for y in range(int(tiles.imag))[10:-10]]
+
+    start = int(argv[1]) if len(argv) >= 2 else 0
+    jump = int(argv[2]) if len(argv) >= 3 else 1
+    horizon = int(argv[3]) if len(argv) >= 4 else 1_000_000
+    for i in range(start, horizon, jump):
+        if i % 10_000 == start:
+            print('.', end='', flush=True)
+        new_robots = {cmod(r[0] + r[1] * i, tiles) for r in robots}
+        if all(v in new_robots for v in vertical):
+            print('\nStep', i)
+            for y in range(int(tiles.imag)):
+                print()
+                for x in range(int(tiles.real)):
+                    c = new_robots.count(x + y*1j)
+                    if c > 0:
+                        print(c, end='')
+                    else:
+                        print('. ', end='')
+            input('Continue')
